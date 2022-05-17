1b0a07ea628b1bd80238499e9b453816916015d2
JINTO N SEBASTIAN <65418412+jinto8301@users.noreply.github.com>
2022-05-17 20:36:04 +0530

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -3,7 +3,7 @@ import time
 from telegram.ext import CommandHandler
 
 from bot import AUTHORIZED_CHATS, dispatcher, updater
-from bot.modules import auth, clone, count, delete, list, permission, shell
+from bot.modules import auth, clone, count, delete, list, permission, shell, scrape
 from bot.helper.telegram_helper.bot_commands import BotCommands
 from bot.helper.telegram_helper.filters import CustomFilters
 from bot.helper.telegram_helper.message_utils import *
@@ -45,7 +45,7 @@ For <i>file</i> results only:
 
 /{BotCommands.CountCommand} [drive_url]: Count data of Drive
 
-/{BotCommands.PermissionCommand} [drive_url] [email]: Set data permission of Drive (Email optional & Only owner)
+/{BotCommands.PermissionCommand} [drive_url]: Set data permission to 'Anyone with the link' (Only owner)
 
 /{BotCommands.DeleteCommand} [drive_url]: Delete data from Drive (Only owner)
 
