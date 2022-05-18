f851ecfc4bc840e30cc2bb0774290cec7dfbe5d3
JINTO N SEBASTIAN <65418412+jinto8301@users.noreply.github.com>
2022-05-18 21:12:47 +0530

    Update scrape.py

--- bot/modules/scrape.py
+++ bot/modules/scrape.py
@@ -10,13 +10,11 @@ from bot.helper.ext_utils.parser import get_gp_link
 @new_thread
 def scrape_gp(update, context):
     buttons = button_builder.ButtonMaker()
-    buttons.buildbutton("JNS BOTS", "https://t.me/JNS_BOTS")
-    buttons.buildbutton("JNS MOVIES", "https://t.me/JNS_MOVIES")
-    reply_markup = InlineKeyboardMarkup(buttons.build_menu(2))
+    reply_markup = InlineKeyboardMarkup(buttons.build_menu(1))
     try:
        query = update.message.text.split()[1]
     except:
-       sendMarkup('<b>send a GPLinks along with this command 👀</b>', context.bot, update, reply_markup)
+       sendMessage('<b>send a GPLinks along with this command 👀</b>', context.bot, update, reply_markup)
        return
  
     if not query.startswith("https://gplinks") or query.startswith("gplinks"):
@@ -29,8 +27,8 @@ def scrape_gp(update, context):
     if not link:      
        sendMessage("Something went wrong\nTry again later..", context.bot, update)
     else:
-       sendMarkup(f"<b>Here is your direct link ⬇️⬇️ \n\n{link} \n\n@JNS_BOTS❤️‍🔥</b>", context.bot, update, reply_markup)
-
+       sendMarkup(f"<b>Here is your direct link ⬇️⬇️\n@JNS_BOTS❤️‍🔥</b>", context.bot, update, reply_markup)
+       buttons.buildbutton("JNS BOTS", link)
 
 gplink_handler = CommandHandler("scrape", scrape_gp,
                                filters=CustomFilters.authorized_chat | CustomFilters.authorized_user, run_async=True)
